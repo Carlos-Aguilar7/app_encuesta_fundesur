08c78c7b5bfe655825327ececc44310bd9b1f318
Carlos-Aguilar7 <carlosaguilar07armas7@gmail.com>
2022-09-06 16:40:25 -0600

    Creando y organizando el proyecto.

---
+++ src/routes/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import {BrowserRouter, Routes, Route } from "react-router-dom"
+
+function App() {
+  return (
+    
+    <BrowserRouter>
+      <Routes>
+        
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
